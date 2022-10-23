f405d1ddc88361ae81b5bcc386f0970af0335547
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-10-24 00:20:12 +0330

    1- GridView.builder 2- shrinkWrap = Creates a scrollable

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,7 +10,80 @@ class Home extends StatelessWidget {
         elevation: 0,
         title: Text('Tic Tac Toe'),
         centerTitle: true,
+        backgroundColor: Colors.grey[900],
       ),
+      backgroundColor: Colors.grey[900],
+      body: Column(children: [
+        getScoreBored(),
+        getGridView(),
+      ],),
+    );
+  }
+
+
+  Widget getGridView() {
+    return GridView.builder(
+      shrinkWrap: true,
+      itemCount: 9,
+      gridDelegate:
+      SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+        );
+      },
+    );
+  }
+
+
+  Widget getScoreBored() {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Player O',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  '0',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Player X',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  '0',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
