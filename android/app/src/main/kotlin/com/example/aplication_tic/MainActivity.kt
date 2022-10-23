266a6dc414bf664e8990fda9bb358a5ca7b3e4ba
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-10-23 16:38:13 +0330

    Start

---
+++ android/app/src/main/kotlin/com/example/aplication_tic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aplication_tic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
