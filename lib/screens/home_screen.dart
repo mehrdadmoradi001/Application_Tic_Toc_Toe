14d08255c6320595ba68599a529f8895435d1792
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-10-24 15:25:22 +0330

    1- change to StatefulWidget 2- getTurn() 3- onTap in GridView.builder 4- tapped(int index)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isTurnO = true;
+
+  List<String> xOroList = ['', '', '', '', '', '', '', '', ''];
 
   @override
   Widget build(BuildContext context) {
@@ -13,32 +22,71 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.grey[900],
       ),
       backgroundColor: Colors.grey[900],
-      body: Column(children: [
-        getScoreBored(),
-        getGridView(),
-      ],),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 20.0),
+            getScoreBored(),
+            SizedBox(height: 40.0),
+            getGridView(),
+            getTurn(),
+          ],
+        ),
+      ),
     );
   }
 
+  Widget getTurn() {
+    return Text(
+      isTurnO ? 'TurnO' : 'TurnX',
+      style: TextStyle(
+          fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
+    );
+  }
 
   Widget getGridView() {
-    return GridView.builder(
-      shrinkWrap: true,
-      itemCount: 9,
-      gridDelegate:
-      SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-          ),
-        );
-      },
+    return Expanded(
+      child: GridView.builder(
+        itemCount: 9,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              tapped(index);
+            },
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+              ),
+              child: Center(
+                child: Text(
+                  xOroList[index],
+                  style: TextStyle(fontSize: 40, color: Colors.white),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 
+  void tapped(int index) {
+    print('$index');
+    setState(() {
+      if (isTurnO && xOroList[index] == '') {
+        xOroList[index] = 'O';
+        isTurnO = false;
+      }
+      if (!isTurnO && xOroList[index] == '') {
+        xOroList[index] = 'X';
+        isTurnO = true;
+      }
+    });
+  }
 
   Widget getScoreBored() {
     return Row(
